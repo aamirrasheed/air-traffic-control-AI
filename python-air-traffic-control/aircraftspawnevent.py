61801230480b172b22dc803de4e0f5627e1c4cd4
asharalam11 <ashar.alam11@gmail.com>
2019-10-09 02:11:26 -0700

    [fix_spawn] Somewhat fixed spawn locations using while loop

--- python-air-traffic-control/aircraftspawnevent.py
+++ python-air-traffic-control/aircraftspawnevent.py
@@ -5,6 +5,11 @@ import random
 
 class AircraftSpawnEvent:
 
+    """
+    Constructor to initialize:
+    1. Spawn Location
+    2. Destination
+    """
     def __init__(self, spawnpoint, destination):
         self.spawnpoint = spawnpoint
         self.destination = destination
@@ -32,52 +37,57 @@ class AircraftSpawnEvent:
     def generateGameSpawnEvents(screen_w, screen_h, destinations):
         randtime = [1]
         randspawnevent = []
-        for x in range(1, conf.get()['game']['n_aircraft']):
+        for x in range(1, conf.get()['game']['n_aircraft']): # Number of aircrafts = 30 default
             randtime.append(random.randint(1, conf.get()['game']['gametime']))
         randtime.sort()
         for x in randtime:
             randspawn, side = AircraftSpawnEvent.__generateRandomSpawnPoint(screen_w, screen_h)
             if (side == 1):
-                def t1(d): 
+                def t1(d):
                     l = d.getLocation()
                     return l[1] > screen_h/2
-                def t2(p1,p2):
+                def t2(p1,p2):                  # What does this do????
                     return 1
             elif (side == 2):
-                def t1(d): 
+                def t1(d):
                     l = d.getLocation()
                     return l[0] < screen_w/2
                 def t2(p1,p2):
                     return 1
             elif (side == 3):
-                def t1(d): 
+                def t1(d):
                     l = d.getLocation()
                     return l[1] < screen_h/2
                 def t2(p1,p2):
                     return 1
             elif (side == 4):
-                def t1(d): 
+                def t1(d):
                     l = d.getLocation()
                     return l[0] > screen_w/2
                 def t2(p1,p2):
                     return 1
             d = AircraftSpawnEvent.valid_destinations(destinations,t1,t2)
             randdest = random.choice(d)
-            randspawnevent.append(AircraftSpawnEvent(randspawn, randdest))
+            randspawnevent.append(AircraftSpawnEvent(randspawn, randdest)) # I am interested in location: randspawn
+        print("Write spawn points")
+        for i in randspawnevent:
+            print(i)
+            print()
         return (randtime, randspawnevent)
 
     @staticmethod
     def __generateRandomSpawnPoint(screen_w, screen_h):
-        side = random.randint(1, 4)
-        previous = 7
+        #side = random.randint(1, 4)     # I guess screen is split into 4 sides (left, right, top, right)
+        side = 4                        # I modified (Have to delete this and remove earlier line)
+        previous = 7                    # Magic Number ?????????????????????????????????
         if side == 1 and side != previous:
-            loc = (random.randint(0, screen_w), 0)
+            loc = (random.randint(0, screen_w), 0)          # Spawn in the top region
         elif side == 2 and side != previous:
-            loc = (screen_w, random.randint(0, screen_h))
+            loc = (screen_w, random.randint(0, screen_h))   # Spawn in the right region
         elif side == 3 and side != previous:
-            loc = (random.randint(0, screen_w), screen_h)
+            loc = (random.randint(0, screen_w), screen_h)   # Spawn in the bottom region
         elif side == 4 and side != previous:
-            loc = (0, random.randint(0, screen_h))
+            loc = (0, random.randint(0, screen_h))          # Spawn in the left region
 
-        previous = side
+        #previous = side               # Have to remove comment
         return (loc), side
