45fe3a32c6eb375eb148a8e8c8a6d86abe804891
John Melloni <johnmelloni@Johns-MacBook-Pro-2.local>
2019-12-05 13:43:01 -0800

    Plots

--- python-air-traffic-control/results.py
+++ python-air-traffic-control/results.py
@@ -2,12 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def displayScores():
-    loss = np.load("score.npy")
+    loss = np.load("episode_125score.npy")
     epochs = np.linspace(0, len(loss), num=len(loss))
+    plt.figure(0)
     plt.title('Scores Over Time')
     plt.xlabel("Epochs")
     plt.ylabel('Score')
     plt.plot(epochs, loss)
+
+    cumsum = np.cumsum(loss)
+    avg = np.divide(cumsum, epochs+1)
+    plt.figure(1)
+    plt.title('Average Scores Over Time')
+    plt.xlabel("Epochs")
+    plt.ylabel('Average Score')
+    plt.plot(epochs, avg)
     plt.show()
 
 if __name__=="__main__":
